49bbc4cfb463e78cac0310ddeaa69d9951857e06
Matt White <mpw73@cornell.edu>
2016-04-22 14:56:09 -0400

    Split js files

--- main.js
+++ main.js
@@ -336,38 +336,6 @@ function plotShots(svg, season) {
   })
 }
 
-function removeCards(){
-  div = $(".player-cards");
-  div.empty();
-}
-
-function playerCards(arr){
-  div = $(".player-cards")
-  console.log(div)
-  arr.forEach(function(e,i,a){
-    console.log(e)
-    var card = makePlayerCard(e[3],377)
-    div.append(card)
-  })
-}
-
-function makePlayerCard(name,id){
-  var html = 
-  "<div class='player-card'>" +
-    "<img id='prof-pic' src='http://stats.nba.com/media/players/230x185/"+id+".png'>" +
-    "<h1>" + name + "</h1>" +
-    "<div class='stats'>" +
-      "<h3>Pts: </h3>" +
-      "<h3 class='pts'>12</h3>" +
-      "<h3>Reb: </h3>" +
-      "<h3 class='reb'>5.4</h3>" +
-      "<h3>Asst: </h3>"
-      "<h3 class='asst'>18.2</h3>" +
-    "</div>" +
-  "</div>" 
-
-  return html
-}
 
 // Create a new circle and then have dragging() handle the rest of the math for 
 // selecting hexbins
